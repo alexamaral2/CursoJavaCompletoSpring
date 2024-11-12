0e5911332ceb94b96cff3f887d07725429b955c3
Alex Jr <alexjunior2018@gmail.com>
2024-11-12 16:44:25 -0300

    initial project

---
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,26 @@
+package aplicacao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import dominio.Pessoa;
+
+public class Programa {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+       
+        Pessoa p = em.find(Pessoa.class, 2);
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+        
+        System.out.println("Pronto");
+        em.close();
+        emf.close();
+    }
+}
