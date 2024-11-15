cfebfbca48869f854c50fa1343af7f259ea70f8e
Alex Jr <alexjunior2018@gmail.com>
2024-11-14 21:06:43 -0300

    finalizando projeto spring h2

--- course/src/main/java/com/alexjr/course/resources/exceptions/ResourceExceptionHandler.java
+++ course/src/main/java/com/alexjr/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.alexjr.course.services.exceptions.DatabaseException;
 import com.alexjr.course.services.exceptions.ResourceNotFoundExeception;
 
 import jakarta.servlet.http.HttpServlet;
@@ -19,6 +20,15 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundExeception e, HttpServletRequest resquest) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				resquest.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest resquest) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
 				resquest.getRequestURI());
 		return ResponseEntity.status(status).body(err);
--- course/src/main/java/com/alexjr/course/services/UserService.java
+++ course/src/main/java/com/alexjr/course/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.alexjr.course.entities.User;
 import com.alexjr.course.repositories.UserRepository;
+import com.alexjr.course.services.exceptions.DatabaseException;
 import com.alexjr.course.services.exceptions.ResourceNotFoundExeception;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -24,19 +29,29 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundExeception(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundExeception(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundExeception(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
@@ -44,5 +59,5 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
-	
+
 }
