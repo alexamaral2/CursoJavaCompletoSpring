8ba8ac77941a0cc9faea20c0f8df766c0a028a5f
Alex Junior <alexjunior2018@gmail.com>
2024-11-18 13:04:34 -0300

    adicionando exemplos

--- workshopmongo/src/main/java/com/alexjr/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/alexjr/workshopmongo/services/UserService.java
@@ -14,23 +14,39 @@ import com.alexjr.workshopmongo.service.exception.ObjectNotFoundException;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository repo;
+	@Autowired
+	private UserRepository repo;
 
-    public List<User> findAll() {
-        return repo.findAll();
-    }
+	public List<User> findAll() {
+		return repo.findAll();
+	}
 
-    public User findById(String id) {
-        Optional<User> user = repo.findById(id);
-        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
-    }
-    
-    public User insert(User obj) {
-    	return repo.insert(obj);
-    }
-    
-    public User fromDTO(UserDTO objDto) {
-    	return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-    }
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = repo.findById(obj.getId()).orElseThrow(() -> new RuntimeException("User not found"));
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
