c9255c217ff5fa584e1ecb9eb650a5d48cebdfa2
Alex Junior <alexjunior2018@gmail.com>
2024-11-18 12:38:11 -0300

    adicionando user por id

--- workshopmongo/src/main/java/com/alexjr/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/alexjr/workshopmongo/services/UserService.java
@@ -7,14 +7,22 @@ import org.springframework.stereotype.Service;
 
 import com.alexjr.workshopmongo.domain.User;
 import com.alexjr.workshopmongo.repository.UserRepository;
+import com.alexjr.workshopmongo.service.exception.ObjectNotFoundException;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
-	
-	@Autowired
-	private UserRepository repo;
-	
-	public List<User> findAll(){
-		return repo.findAll();
-	}
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
 }
