afb6f4b8dd29d3f3259ab52d612e70e6427c8d92
Alex Junior <alexjunior2018@gmail.com>
2024-11-18 12:49:25 -0300

    insercao de usuario com post

--- workshopmongo/src/main/java/com/alexjr/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/alexjr/workshopmongo/services/UserService.java
@@ -1,15 +1,15 @@
 package com.alexjr.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alexjr.workshopmongo.domain.User;
+import com.alexjr.workshopmongo.dto.UserDTO;
 import com.alexjr.workshopmongo.repository.UserRepository;
 import com.alexjr.workshopmongo.service.exception.ObjectNotFoundException;
-
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -24,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
+    
+    public User insert(User obj) {
+    	return repo.insert(obj);
+    }
+    
+    public User fromDTO(UserDTO objDto) {
+    	return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
